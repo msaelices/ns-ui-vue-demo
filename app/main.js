7b3279e4e8414d7dc1a3aac8929dc7ff18cbb146
msaelices <msaelices@gmail.com>
2018-09-16 01:34:41 +0200

    Add a RadSideDrawer sidebar.

--- app/main.js
+++ app/main.js
@@ -1,6 +1,9 @@
 import Vue from 'nativescript-vue';
 import RadListView from 'nativescript-ui-listview/vue';
 
+// TODO: Change to Vue.use(RadSideDrawer) when next version was released
+import RadSideDrawer from 'nativescript-ui-sidedrawer/vue';
+
 Vue.use(RadListView);
 
 import * as views from './views';
--- app/views/Home.vue
+++ app/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <Page class="page">
     <ActionBar class="action-bar" title="Home">
+      <ActionItem text="Menu" @tap="$refs.drawer.nativeView.showDrawer()"/>
     </ActionBar>
     <StackLayout>
-      <RadListView ref="listView"
-                   for="item in itemList"
-                   @itemTap="onItemTap">
-        <v-template>
-          <StackLayout class="item" orientation="vertical">
-            <Label class="big" :text="item.name"></Label>
-            <Label :text="item.description"></Label>
-          </StackLayout>
-        </v-template>
-      </RadListView>
+      <RadSideDrawer ref="drawer">
+        <StackLayout ~drawerContent backgroundColor="white">
+          <Label text="Drawer" />
+        </StackLayout>
+        <StackLayout ~mainContent>
+          <RadListView ref="listView"
+                       for="item in itemList"
+                       @itemTap="onItemTap">
+            <v-template>
+              <StackLayout class="item" orientation="vertical">
+                <Label class="big" :text="item.name"></Label>
+                <Label :text="item.description"></Label>
+              </StackLayout>
+            </v-template>
+          </RadListView>
+        </StackLayout>
+      </RadSideDrawer>
     </StackLayout>
   </Page>
 </template>
