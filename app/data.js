3e755f2820143e581cad3d48624d2d1af9103092
Manuel Saelices <msaelices@gmail.com>
2019-01-14 12:54:01 +0100

    Remove not used data.

--- app/data.js
+++ app/data.js
@@ -72,10 +72,10 @@ export class MovieConverter {
 
 export const getCountriesData = () => {
   return new ObservableArray([
-    { Country: 'Germany', Amount: 15, SecondVal: 14, ThirdVal: 24, Impact: 0, Year: 0 },
-    { Country: 'France', Amount: 13, SecondVal: 23, ThirdVal: 25, Impact: 0, Year: 0 },
-    { Country: 'Bulgaria', Amount: 24, SecondVal: 17, ThirdVal: 23, Impact: 0, Year: 0 },
-    { Country: 'Spain', Amount: 11, SecondVal: 19, ThirdVal: 24, Impact: 0, Year: 0 },
-    { Country: 'USA', Amount: 18, SecondVal: 8, ThirdVal: 21, Impact: 0, Year: 0 }
+    { Country: 'Germany', Amount: 15 },
+    { Country: 'France', Amount: 13 },
+    { Country: 'Bulgaria', Amount: 24 },
+    { Country: 'Spain', Amount: 11 },
+    { Country: 'USA', Amount: 18 }
   ])
 }
