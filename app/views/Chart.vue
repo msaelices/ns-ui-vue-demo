e09209aa421cdaaf5d3b362efc4e5fbb506424ec
Manuel Saelices <msaelices@gmail.com>
2019-01-29 00:38:03 +0100

    Forgot to upload this Chart.vue file.

---
+++ app/views/Chart.vue
@@ -0,0 +1,34 @@
+<template>
+  <Page class="page">
+    <ActionBar
+      color="white"
+      backgroundColor="#53ba82"
+      class="action-bar"
+      title="Chart demo">
+    </ActionBar>
+    <RadCartesianChart>
+      <BarSeries v-tkCartesianSeries
+                 categoryProperty="Country"
+                 valueProperty="Amount"
+                 :items="items" />
+      <CategoricalAxis v-tkCartesianHorizontalAxis />
+      <LinearAxis v-tkCartesianVerticalAxis />
+    </RadCartesianChart>
+  </Page>
+</template>
+
+<script>
+import { getCountriesData } from '../data';
+
+export default {
+  name: 'chart-view',
+  data() {
+    return {
+      items: getCountriesData(),
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
